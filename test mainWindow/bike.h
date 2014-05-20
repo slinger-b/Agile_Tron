f4eef34d32b2900b0b145f3cdff2cdac756a2d35
slinger-b <slinger-b@hotmail.com>
2014-05-20 09:45:58 +0200

    test initialisation of the mainWindow + image
    Problem to incorporate image with relative part. So to have the image we
    have to manually set the entired path

---
+++ test mainWindow/bike.h
@@ -0,0 +1,53 @@
+#ifndef BIKE_H
+#define BIKE_H
+
+#include <liste.h>
+
+enum dir {HAUT, DROITE, BAS, GAUCHE, RIEN};
+
+void ajoute_avance(int pos[2], dir direct, int vit);
+
+class Bike
+{
+public:
+
+    Bike(); //constructeur par defaut
+    Bike(int pos_depart[], int vitesse_depart, dir dir_depart);
+    Bike(Bike const& BikeACopier); // Le prototype du constructeur de copie
+    ~Bike();
+
+    void chgDir (dir nv_dir);
+    void avance();
+    void traverseMur();
+
+    void gagne();
+    void perd();
+    void perduCollision();
+
+    void reinitialise();
+
+    int get_vitesse()const;
+    Liste* get_liste()const;
+    dir get_dir()const;
+    dir get_invers_dir()const;
+    int getScore()const{return score;}
+    bool getPerdu()const{return perdu;}
+
+private:
+
+    int posDep[2];
+    int vitDep;
+    dir dirDep;
+
+    Liste* PosSuccessives;
+
+    int vitesse;
+    dir direction;
+
+    bool chgmt_dir;
+
+    int score;
+    bool perdu;
+};
+
+#endif // BIKE_H
---
+++ test mainWindow/testliste.cpp
@@ -0,0 +1,110 @@
+#include "liste.h"
+#include "node.h"
+#include <vector> //pour cr√©er des vecteurs !!! (allocation dynamique facile!!!
+
+#include <iostream>
+
+#include <QtTest/QtTest>
+
+class TestListe: public QObject
+{
+        Q_OBJECT
+
+private slots:
+
+    //Fonctions liÈes ‡ la liste en elle-mÍme
+    void push_and_top();
+    void delete_top_and_pop();
+
+    ////Fonctions liÈes ‡ la lecture de la liste
+    void getElem_suivant_premier_getNextElem();
+
+private:
+    Node* node1,node2;
+    int pos1 [2],pos2 [2],posTchek [2];
+};
+
+
+void TestListe::push_and_top()
+{
+        pos1[0]= 4;
+        pos1[1]= 10;
+
+        Liste* Liste1 = new Liste();
+        Liste1->push(pos1);
+
+        Liste1->top(posTchek);
+
+        QVERIFY(posTchek[0]==pos1[0]);
+        QVERIFY(posTchek[1]==pos1[1]);
+
+        delete Liste1;
+}
+
+void TestListe::delete_top_and_pop()
+{
+        pos1[0]= 4;
+        pos1[1]= 10;
+        pos2[0]= 11;
+        pos2[1]= 11;
+
+        Liste* Liste1 = new Liste();
+        Liste1->push(pos1); // put 2 new node in the liste
+        Liste1->push(pos2);
+
+        Liste1->delete_top(); //remove the last node
+        Liste1->top(posTchek);
+
+        QVERIFY(posTchek[0]==pos1[0]); //tchek if the top is the first node
+        QVERIFY(posTchek[1]==pos1[1]);
+        Liste1->push(pos2); //reput the second node
+
+        Liste1->pop(posTchek);
+        QVERIFY(posTchek[0]==pos2[0]); //tchek if the position is the old first node
+        QVERIFY(posTchek[1]==pos2[1]);
+
+        Liste1->top(posTchek);
+
+        QVERIFY(posTchek[0]==pos1[0]); //tchek if the old first node was removed
+        QVERIFY(posTchek[1]==pos1[1]);
+
+        delete Liste1;
+}
+void TestListe::getElem_suivant_premier_getNextElem()
+{
+        pos1[0]= 4;
+        pos1[1]= 10;
+        pos2[0]= 11;
+        pos2[1]= 11;
+
+        Liste* Liste1 = new Liste();
+        Liste1->push(pos1); // put 2 new node in the liste
+        Liste1->push(pos2);
+
+        Liste1->getElem(posTchek);
+        QVERIFY(posTchek[0]==pos1[0]); //tchek getElem
+        QVERIFY(posTchek[1]==pos1[1]);
+
+        Liste1->premier();
+        Liste1->getElem(posTchek);
+
+        QVERIFY(posTchek[0]==pos2[0]); //tchek if position Head = position Elem
+        QVERIFY(posTchek[1]==pos2[1]);
+
+        Liste1->getNextElem(posTchek);
+
+        QVERIFY(posTchek[0]==pos1[0]); //tchek getNext_Elem
+        QVERIFY(posTchek[1]==pos1[1]);
+
+        Liste1->suivant();
+        Liste1->getElem(posTchek);
+
+        QVERIFY(posTchek[0]==pos1[0]); //tchek suivant
+        QVERIFY(posTchek[1]==pos1[1]);
+
+        delete Liste1;
+}
+
+QTEST_MAIN(TestListe)
+#include "testliste.moc"
+
